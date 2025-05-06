eadca0d8388c27c2b36bb93e970230b487ffb792
Nweneary Uzochukwu Winnie <uzochukwuwinnie@gmail.com>
2025-05-06 19:18:47 +0100

    Create the Hero and Skills sections (#1)
    * Change page title
    
    * Add logos
    
    * Change favicon
    
    * Install react icons
    
    * Add profile picture
    
    * Add custom font
    
    * Add data for skills
    
    * Import custom font
    
    * Create navbar
    
    * Create hero section
    
    * Create skills section
    
    * Add all section to app root

---
+++ src/components/Skills.tsx
@@ -0,0 +1,20 @@
+import SkillData from "../data/skillsData.ts";
+import SkillCard from "./micro/SkillCard";
+import type { ReactNode } from "react";
+
+const Skills = () => {
+  return (
+    <section className="text-white py-10 my-10">
+      <h3 className="text-2xl text-center font-semibold">Skills</h3>
+      <section className="flex flex-wrap gap-4 justify-center max-w-[600px] mx-auto my-10">
+        {SkillData.map(
+          (skill: { name: string; icon: ReactNode }, index: number) => (
+            <SkillCard key={index} icon={skill.icon} name={skill.name} />
+          )
+        )}
+      </section>
+    </section>
+  );
+};
+
+export default Skills;
