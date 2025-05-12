db84eb05ea1b0796765df47e6ffea493617dcf6c
Wineshuga <uzochukwuwinnie@gmail.com>
2025-05-12 22:24:00 +0100

    Adjust skills font size

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,7 +5,7 @@ import type { ReactNode } from "react";
 const Skills = () => {
   return (
     <section className="text-white py-10 my-10">
-      <h3 className="text-2xl text-center font-semibold">Skills</h3>
+      <h3 className="md:text-4xl text-2xl text-center font-semibold">Skills</h3>
       <section className="flex flex-wrap gap-4 justify-center max-w-[600px] mx-auto my-10">
         {SkillData.map(
           (skill: { name: string; icon: ReactNode }, index: number) => (
