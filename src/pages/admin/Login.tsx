3fcc6bd8e25ad845a86d999d2668b152c4c14d07
Wineshuga <uzochukwuwinnie@gmail.com>
2026-01-06 00:06:30 +0100

    Create login page for admin

---
+++ src/pages/admin/Login.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      setIsLoading(true);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      localStorage.setItem(
+        "isAuth",
+        JSON.stringify({ user: userCredential.user })
+      );
+      navigate("/admin/dashboard");
+    } catch (error) {
+      console.error("Error during sign-in:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md p-8 rounded-lg shadow-lg bg-white">
+        <h1 className="text-3xl font-bold mb-6 text-center font-mono">Login</h1>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none"
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none"
+          />
+
+          <button
+            type="submit"
+            className="w-full py-2 rounded-lg font-semibold border"
+            disabled={isLoading}
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
