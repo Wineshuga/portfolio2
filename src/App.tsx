b09b1411449292fd77307e187a99f6794cfde55c
Wineshuga <uzochukwuwinnie@gmail.com>
2026-01-06 00:59:27 +0100

    Add routes to other pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,27 @@
 import { useEffect } from "react";
-import Contact from "./components/Contact";
-import Experience from "./components/Experience";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/admin/Login";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Home from "./pages/Home";
+import Editor from "./pages/admin/Editor";
+import BlogList from "./pages/blog/BlogList";
+import BlogPost from "./pages/blog/BlogPost";
 
 function App() {
   useEffect(() => {
     AOS.init();
   });
   return (
-    <>
-      <Navbar />
-      <main className="bg-black p-4 font-nunito">
-        <Hero />
-        <Skills />
-        <Experience />
-        <Projects />
-        <Contact />
-        <Footer />
-      </main>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/admin/login" element={<Login />} />
+        <Route path="/admin/dashboard" element={<Editor />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<BlogList />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
+      </Routes>
+    </Router>
   );
 }
 
