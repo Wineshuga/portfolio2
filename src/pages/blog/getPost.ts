aa7e42efe3b5c39a20907cf90a115484a22d9cea
Wineshuga <uzochukwuwinnie@gmail.com>
2026-01-06 01:55:55 +0100

    Get all posts

---
+++ src/pages/blog/getPost.ts
@@ -0,0 +1,27 @@
+import { collection, getDocs, query, Timestamp, where } from "firebase/firestore";
+import { db } from "../../firebase";
+
+interface PostType {
+  id: string;
+  title: string;
+  slug: string;
+  excerpt: string;
+  content: string;
+  createdAt: Timestamp;
+  published: boolean;
+}
+
+export const getPosts = async (): Promise<PostType[]> => {
+  const q = query(collection(db, "posts"), where("published", "==", true));
+  const snapshot = await getDocs(q);
+  
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    title: doc.data().title,
+    slug: doc.data().slug,
+    excerpt: doc.data().excerpt,
+    content: doc.data().content,
+    createdAt: doc.data().createdAt,
+    published: doc.data().published,
+  }));
+};
