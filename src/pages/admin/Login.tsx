2a965223336972e9db7cd9d21cfd328ab2ba0bda
Wineshuga <uzochukwuwinnie@gmail.com>
2026-01-14 23:09:59 +0100

    Restyle login page

--- src/pages/admin/Login.tsx
+++ src/pages/admin/Login.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
+import { GoEye, GoEyeClosed } from "react-icons/go";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const navigate = useNavigate();
   const handleSubmit = async (e: React.FormEvent) => {
@@ -25,7 +27,9 @@ const Login = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="w-full max-w-md p-8 rounded-lg shadow-lg bg-white">
-        <h1 className="text-3xl font-bold mb-6 text-center font-mono">Login</h1>
+        <h1 className="text-3xl font-bold mb-6 text-center font-nunito">
+          Welcome
+        </h1>
 
         <form onSubmit={handleSubmit} className="space-y-4">
           <input
@@ -33,23 +37,32 @@ const Login = () => {
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none"
+            className="w-full px-4 text-sm py-2 border focus:outline-none"
           />
 
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none"
-          />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-4  text-sm py-2 border focus:outline-none"
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword((prev) => !prev)}
+              className="cursor-pointer absolute bottom-3 right-3"
+            >
+              {showPassword ? <GoEyeClosed /> : <GoEye />}
+            </button>
+          </div>
 
           <button
             type="submit"
-            className="w-full py-2 rounded-lg font-semibold border"
+            className="w-full py-2 rounded-lg text-sm font-semibold border border-[#e1d3b6] cursor-pointer"
             disabled={isLoading}
           >
-            Sign In
+            {isLoading ? "Logging in..." : "Log In"}
           </button>
         </form>
       </div>
